eb2100ff776b8d6d2638f570fd00459270e4c0aa
suasnomy <bhandarisudhir4274@gmail.com>
2023-03-19 11:05:27 +0530

    firebase setup

---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'homePage',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent {
+
+}
